9fc350f105dbb1adee6d514748a3fd1de5516331
Robson Sousa <robsoncasousa@gmail.com>
2019-09-09 14:27:38 -0300

    feat: starting the text robot

--- robots/text.js
+++ robots/text.js
@@ -1,10 +1,11 @@
 const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+const setenceBoundaryDetection = require('sbd')
 
 async function robot(content){
 	await fetchContentFromWikipedia(content)
-//async anitizeContent(content)
-//async reakContentIntoSentences(content)
+	sanitizeContent(content)
+	breakContentIntoSentences(content)
 	
 	async function fetchContentFromWikipedia(content){
 		const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
@@ -13,6 +14,42 @@ async function robot(content){
 		const wikipediaContent = wikipediaResponse.get()
 
 		content.sourceContentOriginal = wikipediaContent.content
+	}
+
+	function sanitizeContent(content){
+		const withoutBlankLinesAndMarkdowns = removeBlankLinesAndMarkdowns(content.sourceContentOriginal)
+		const withoutDateInParentheses = removeDateInParentheses(withoutBlankLinesAndMarkdowns)
+
+		content.sourceContentSanitized = withoutDateInParentheses
+
+		function removeBlankLinesAndMarkdowns(text){
+			const allLines = text.split('\n')
+			const withoutBlankLinesAndMarkdowns = allLines.filter( (line) => {
+				if(line.trim().length === 0 || line.trim().startsWith('=') ){
+					return false
+				}
+				return true
+			})
+
+			return withoutBlankLinesAndMarkdowns.join(' ')
+		}
+
+		function removeDateInParentheses(text){
+			return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
+		}
+	}
+	function breakContentIntoSentences(content){
+		content.sentences = []
+
+		const sentences = setenceBoundaryDetection.sentences(content.sourceContentSanitized)
+		sentences.forEach( (sentence) => {
+			content.sentences.push({
+				text: sentence,
+				keywords: [],
+				images: []
+			})
+		})
+		console.log(content)
 	}
 }
 
